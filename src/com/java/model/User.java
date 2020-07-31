7b9ad518fc2dd8e4c6eb583a10e05c30f2fdcd7a
yy <yy@qq.com>
2020-07-31 14:18:31 +0800

    3

--- src/com/java/model/User.java
+++ src/com/java/model/User.java
@@ -31,13 +31,23 @@ public class User {
 
     private Integer id;
     private String name;
+    private String phone;
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
                 '}';
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public Integer getId() {
